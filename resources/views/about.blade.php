03020cd3f9c4cfecfcb648621fc8d4888d0b1278
MuhammadAzkaQinthara <m.azka.qinthara@gmail.com>
2021-07-28 11:26:27 +0700

    2

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,20 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Azka | About</title>
-</head>
-<body>
-    <h1>Halaman About</h1>
-    <h3>@php
-        echo $name;
-    @endphp</h3>
-    <p>@php
-        echo $email;
-    @endphp</p>
-    <img src="img/<?= $image; ?>" alt="<?= $name; ?>" width="200">
-</body>
-</html>
+@extends('layouts.main')
+
+@section('container')
+<h1>Halaman About</h1>
+<h3>{{ $name }}</h3>
+<p>{{ $email }}</p>
+<img src="img/{{ $image }}" alt="{{ $name }}" width="200">
+@endsection
